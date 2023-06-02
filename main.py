ca4f7fd07d85137c7ce70d619925a9814cea1daf
Fahim Bashar <74380584+Keilwerth11270@users.noreply.github.com>
2023-06-02 19:40:08 -0400

    Game complete
    The game of life has been finished with the addition of a way to speed up or slow down the game, as well as a way to reset the screen in case you want to start over.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import numpy as np
 #Will add my own components to this project.
 #Planned: Way to control tick rate, reset screen, and possibly more
 
-COLOR_BACKGROUND = (10,10,10)
+COLOR_BACKGROUND = (150,30,140)
 COLOR_GRID = (40,40,40)
 COLOR_DIE = (170,170,170)
 COLOR_ALIVE = (255,255,255)
@@ -43,26 +43,51 @@ def update(screen, cells, size, with_progress=False):
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption("Conway's Game of Life")
+
+
+    sleeptime = .001
     
     cells = np.zeros((60, 80))
     screen.fill(COLOR_GRID)
     update(screen, cells, 10)
 
-    pygame.display.flip()
     pygame.display.update()
 
     running = False
 
     while True:
+
         for event in pygame.event.get():
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+            
             elif event.type == pygame.KEYDOWN:
+
                 if event.key == pygame.K_SPACE:
+                    if running:
+                        sleeptime = .001 #on pause, resets the tick rate because slow ticks means slower click input
                     running = not running
                     update(screen, cells, 10)
                     pygame.display.update()
+                
+                pressed = pygame.key.name(event.key)
+
+                if pressed == 'r': #clear screen
+                    cells = np.zeros((60, 80))
+                    update(screen, cells, 10)
+                    pygame.display.update()
+
+                if pressed == 'q': #decrease tickrate
+                    sleeptime += .025
+
+                if pressed == 'w': #increase tickrate
+                    if sleeptime > 0.001: sleeptime -= .025
+
+                
+
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
                 cells[pos[1] // 10, pos[0] // 10] = 1
@@ -75,7 +100,7 @@ def main():
             cells = update(screen, cells, 10, with_progress = True)
             pygame.display.update()
 
-        time.sleep(0.001)
+        time.sleep(sleeptime)
 
 if __name__ == '__main__':
     main()
